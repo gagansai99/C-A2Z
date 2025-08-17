c9461009f308eda6642da10826572f7c14c11c3a
gagansai99 <gagansaialasapuri99@gmail.com>
2025-08-17 11:09:22 +0530

    Started with IO, Conditional statements and datatypes

---
+++ Learn the basics/Basic c++/Datatypes.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    long m;
+    bool flag = true;
+    float z;
+    double d;
+    char ch='a';
+    string a="Gagan";
+    cout<<a;
+}
---
+++ Learn the basics/Basic c++/UserInputOutput.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"Gagan"<<"\n";
+    string name;
+    cout<<"Enter your name:";
+    cin>>name;
+    cout<<"Your name is "<<name;
+}
