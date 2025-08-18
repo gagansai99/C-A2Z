126293e89458ff08e96b934feadd297cbff2c622
gagansai99 <gagansaialasapuri99@gmail.com>
2025-08-18 14:21:52 +0530

    Patterns from 1 to 9

---
+++ Learn the basics/Patterns/Pattern9.cpp
@@ -0,0 +1,28 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=2*i-1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i-1;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=2*(n - i) + 1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+
+    return 0;
+}
